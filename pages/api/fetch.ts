6ff7c65318f65b99dc2870823c85e38d582cad27
hartaithan <hartaithan@gmail.com>
2023-01-15 13:57:26 +0900

    remove puppeteer stealth plugin

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -1,39 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer-extra";
+// @ts-ignore
+import puppeteer from "puppeteer-core";
 import { load } from "cheerio";
 import chrome from "chrome-aws-lambda";
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-
-import "puppeteer-extra-plugin-user-data-dir";
-import "puppeteer-extra-plugin-user-preferences";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.app";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.csi";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.runtime";
-import "puppeteer-extra-plugin-stealth/evasions/defaultArgs";
-import "puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow";
-import "puppeteer-extra-plugin-stealth/evasions/media.codecs";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.languages";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.permissions";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.plugins";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.vendor";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.webdriver";
-import "puppeteer-extra-plugin-stealth/evasions/sourceurl";
-import "puppeteer-extra-plugin-stealth/evasions/user-agent-override";
-import "puppeteer-extra-plugin-stealth/evasions/webgl.vendor";
-import "puppeteer-extra-plugin-stealth/evasions/window.outerdimensions";
 
 type Data = {
   title: string;
 };
 
-puppeteer.use(StealthPlugin());
-
 const local = {
   args: [],
   executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-  headless: false,
+  headless: true,
 };
 
 const getBrowser = async () => {
@@ -60,17 +38,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     "Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10"
   );
 
-  await page.goto(url);
-  await page.waitForTimeout(5000);
+  if (!url) {
+    return res.status(400).json({ title: "url not found" });
+  }
+
+  await page.goto(url as string);
 
   const content = await page.content();
   const cheerio = load(content);
 
   const title = cheerio("title").text();
 
-  await browser.close();
-
-  res.status(200).json({ title });
+  return res.status(200).json({ title });
 };
 
 export default handler;
