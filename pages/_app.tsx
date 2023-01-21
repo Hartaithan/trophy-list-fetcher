f66ef97f79246a32cd32d141add354c2ee93bb99
hartaithan <hartaithan@gmail.com>
2023-01-21 13:04:56 +0900

    home page clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { FC } from "react";
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const inter = Inter({ subsets: ["latin"] });
+
+const App: FC<AppProps> = (props) => {
+  const { Component, pageProps } = props;
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        * {
+          font-family: ${inter.style.fontFamily};
+          font-style: ${inter.style.fontStyle};
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default App;
