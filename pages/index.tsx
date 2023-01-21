54af5c7242eeef662d454ceea13986608c29ce95
hartaithan <hartaithan@gmail.com>
2023-01-21 14:08:29 +0900

    add home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import { NextPage } from "next";
+import { FormEventHandler } from "react";
+import { useRouter } from "next/router";
 
-const Home = () => {
+interface FormTarget extends EventTarget {
+  url: { value: string };
+}
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const form = e.target as FormTarget;
+    const url = form.url.value.trim();
+    if (url.length === 0) {
+      alert("URL field is empty!");
+      return;
+    }
+    router.push({
+      pathname: "/result",
+      query: {
+        url,
+      },
+    });
+  };
+
   return (
     <>
       <Head>
@@ -10,7 +35,12 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>Hello world!</main>
+      <main className={styles.main}>
+        <form className={styles.form} onSubmit={onSubmit}>
+          <input className={styles.input} name="url" placeholder="Enter URL" />
+          <input className={styles.input} value="Submit" type="submit" />
+        </form>
+      </main>
     </>
   );
 };
