f004d6c189e906fb61321c7d7a39fb436a449bf0
hartaithan <hartaithan@gmail.com>
2023-01-22 12:27:39 +0900

    add table rows toggling

--- pages/result.tsx
+++ pages/result.tsx
@@ -4,7 +4,7 @@ import {
   InferGetServerSidePropsType,
   NextPage,
 } from "next";
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -29,6 +29,45 @@ interface IResult {
 interface IResultPageProps {
   result: IResult;
 }
+
+interface IRow {
+  id: number;
+  value: string;
+  label: string;
+}
+
+const options: IRow[] = [
+  {
+    id: 1,
+    value: "complete",
+    label: "Complete",
+  },
+  {
+    id: 2,
+    value: "name",
+    label: "Trophy name",
+  },
+  {
+    id: 3,
+    value: "description",
+    label: "Trophy description",
+  },
+  {
+    id: 4,
+    value: "list_name",
+    label: "List name",
+  },
+  {
+    id: 5,
+    value: "type",
+    label: "Trophy type",
+  },
+  {
+    id: 6,
+    value: "is_base",
+    label: "Is trophy is base game",
+  },
+];
 
 export const getServerSideProps: GetServerSideProps<IResultPageProps> = async (
   ctx
@@ -57,9 +96,31 @@ const ResultPage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = (props) => {
   const [complete, setComplete] = useState(false);
+  const [rows, setRows] = useState<IRow[]>([...options]);
 
   const onChange = () => {
     setComplete((prev) => !prev);
+  };
+
+  const addRow: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    const option = options.find((opt) => opt.value === e.target.value);
+    if (!option) {
+      alert("option not found");
+      return;
+    }
+    const isAlreadySelected = rows.some((row) => row.value === option.value);
+    if (isAlreadySelected) {
+      alert("row is already showed");
+    } else {
+      const editedRows = [...rows, option].sort((a, b) => a.id - b.id);
+      setRows(editedRows);
+    }
+    e.target.value = "";
+  };
+
+  const deleteRow = (row: IRow) => {
+    const filtered = [...rows].filter((item) => item.id !== row.id);
+    setRows(filtered);
   };
 
   if (!props.result.lists) {
@@ -77,24 +138,66 @@ const ResultPage: NextPage<
         <h3 className={styles.title}>{props.result.title}</h3>
         <input type="checkbox" checked={complete} onChange={onChange} />
       </div>
+      <select
+        className={styles.select}
+        placeholder="Select table rows"
+        onChange={addRow}
+      >
+        <option
+          disabled
+          hidden
+          selected
+          label="Select table rows"
+          defaultValue={undefined}
+        />
+        {options.map((option) => (
+          <option key={option.id} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <div className={styles.rows}>
+        {rows.map((row) => (
+          <div className={styles.row} key={row.id}>
+            {row.label}
+            <p className={styles.rowDelete} onClick={() => deleteRow(row)}>
+              ✖
+            </p>
+          </div>
+        ))}
+      </div>
       <table className={styles.table}>
-        {props.result.lists.map((list) =>
-          list.trophies.map((trophy, index) => {
-            if (trophy.type === "Platinum") {
-              return null;
-            }
-            return (
-              <tr key={trophy.name + index}>
-                <td>{complete ? "YES" : "NO"}</td>
-                <td>{trophy.name}</td>
-                <td>{trophy.description}</td>
-                <td>{list.name}</td>
-                <td>{trophy.type}</td>
-                <td>{list.name === "Base Game" ? "YES" : "NO"}</td>
-              </tr>
-            );
-          })
-        )}
+        <tbody>
+          {props.result.lists.map((list) =>
+            list.trophies.map((trophy, index) => {
+              if (trophy.type === "Platinum") {
+                return null;
+              }
+              return (
+                <tr key={trophy.name + index}>
+                  {rows.some((row) => row.value === options[0].value) && (
+                    <td>{complete ? "YES" : "NO"}</td>
+                  )}
+                  {rows.some((row) => row.value === options[1].value) && (
+                    <td>{trophy.name}</td>
+                  )}
+                  {rows.some((row) => row.value === options[2].value) && (
+                    <td>{trophy.description}</td>
+                  )}
+                  {rows.some((row) => row.value === options[3].value) && (
+                    <td>{list.name}</td>
+                  )}
+                  {rows.some((row) => row.value === options[4].value) && (
+                    <td>{trophy.type}</td>
+                  )}
+                  {rows.some((row) => row.value === options[5].value) && (
+                    <td>{list.name === "Base Game" ? "YES" : "NO"}</td>
+                  )}
+                </tr>
+              );
+            })
+          )}
+        </tbody>
       </table>
     </>
   );
