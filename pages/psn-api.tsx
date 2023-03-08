1f0cfd88028ae14729663ec36350c46b51499061
Hartaithan <hartaithan@gmail.com>
2023-03-08 14:09:02 +0900

    exchange npsso to access token test

--- pages/psn-api.tsx
+++ pages/psn-api.tsx
@@ -1,5 +1,38 @@
+import Input from "@/components/Input";
 import { NextPage } from "next";
+import {
+  exchangeNpssoForCode,
+  exchangeCodeForAccessToken,
+  AuthTokensResponse,
+} from "psn-api";
+import { useState } from "react";
 
-const PSNPage: NextPage = () => <p>psn-api page</p>;
+const PSNPage: NextPage = () => {
+  const [auth, setAuth] = useState<AuthTokensResponse>(
+    {} as AuthTokensResponse
+  );
+  const [npsso, setNpsso] = useState("");
+
+  const handleSubmit = async () => {
+    console.info("npsso", npsso);
+    const accessCode = await exchangeNpssoForCode(npsso);
+    console.info("accessCode", accessCode);
+    const authorization = await exchangeCodeForAccessToken(accessCode);
+    console.info("authorization", authorization);
+    setAuth(authorization);
+  };
+
+  return (
+    <>
+      <p style={{ marginBottom: 10 }}>Authorization: {JSON.stringify(auth)}</p>
+      <Input
+        value={npsso}
+        placeholder="Enter your NPSSO token"
+        onChange={(e) => setNpsso(e.target.value)}
+      />
+      <Input type="button" value="Submit" onClick={() => handleSubmit()} />
+    </>
+  );
+};
 
 export default PSNPage;
