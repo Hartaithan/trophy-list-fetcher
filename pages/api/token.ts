5753eb5723408e688a2886b63af2e562833c8b4f
Hartaithan <hartaithan@gmail.com>
2023-03-08 15:10:18 +0900

    handle api/token errors

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { exchangeNpssoForCode, exchangeCodeForAccessToken } from "psn-api";
+import {
+  exchangeNpssoForCode,
+  exchangeCodeForAccessToken,
+  AuthTokensResponse,
+} from "psn-api";
 
 interface ITokenQueries {
   [key: string]: string | string[];
@@ -9,8 +13,22 @@ interface ITokenQueries {
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { npsso } = req.query as ITokenQueries;
 
-  const accessCode = await exchangeNpssoForCode(npsso);
-  const authorization = await exchangeCodeForAccessToken(accessCode);
+  let accessCode: string | null = null;
+  let authorization: AuthTokensResponse | null = null;
+
+  try {
+    accessCode = await exchangeNpssoForCode(npsso);
+  } catch (error) {
+    console.error("exchange access code error", error);
+    return res.status(400).json({ message: "unable to get access code" });
+  }
+
+  try {
+    authorization = await exchangeCodeForAccessToken(accessCode);
+  } catch (error) {
+    console.error("exchange access token error", error);
+    return res.status(400).json({ message: "unable to get access token" });
+  }
 
   return res.status(200).json({ authorization });
 };
