dd35e0a8b72361cdd77457d8e266aa52ad997812
Hartaithan <hartaithan@gmail.com>
2023-03-11 16:43:53 +0900

    refresh tokens with middleware

--- middleware.ts
+++ middleware.ts
@@ -3,20 +3,63 @@ import {
   getProfileFromUserName,
   AuthorizationPayload,
   ProfileFromUserNameResponse,
+  exchangeRefreshTokenForAuthTokens,
+  AuthTokensResponse,
 } from "psn-api";
+
+type NullableProfileResponse = ProfileFromUserNameResponse | null;
+type NullableAuthResponse = AuthTokensResponse | null;
+
+const getProfile = async (token: string): Promise<NullableProfileResponse> => {
+  const authorization: AuthorizationPayload = { accessToken: token };
+  let response: NullableProfileResponse = null;
+  try {
+    response = await getProfileFromUserName(authorization, "me");
+  } catch (error) {
+    console.error("unable to get profile", error);
+  }
+  return response;
+};
+
+const refreshTokens = async (
+  res: NextResponse,
+  token: string
+): Promise<NullableAuthResponse> => {
+  let authorization: NullableAuthResponse = null;
+  try {
+    authorization = await exchangeRefreshTokenForAuthTokens(token);
+  } catch (error) {
+    console.error("unable to refresh tokens", error);
+  }
+  if (authorization) {
+    const { accessToken, expiresIn, refreshToken, refreshTokenExpiresIn } =
+      authorization;
+    res.cookies.set("access_token", accessToken, { maxAge: expiresIn });
+    res.cookies.set("refresh_token", refreshToken, {
+      maxAge: refreshTokenExpiresIn,
+    });
+  }
+  return authorization;
+};
 
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
 
-  const access_token = req.cookies.get("access_token")?.value;
-  let response: ProfileFromUserNameResponse | null = null;
+  let access_token = req.cookies.get("access_token")?.value;
+  let refresh_token = req.cookies.get("refresh_token")?.value;
+
+  let response: NullableProfileResponse = null;
 
   if (access_token) {
-    const authorization: AuthorizationPayload = { accessToken: access_token };
-    try {
-      response = await getProfileFromUserName(authorization, "me");
-    } catch (error) {
-      console.error("unable to get profile", error);
+    response = await getProfile(access_token);
+  }
+
+  if (!access_token && refresh_token) {
+    const auth = await refreshTokens(res, refresh_token);
+    if (auth) {
+      access_token = auth.accessToken;
+      refresh_token = auth.refreshToken;
+      response = await getProfile(access_token);
     }
   }
 
