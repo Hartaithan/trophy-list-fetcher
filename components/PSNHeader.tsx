6d7140d8140f7ef1d25c9878692f231d97c93387
Hartaithan <hartaithan@gmail.com>
2023-03-18 12:09:47 +0900

    add signOut button

--- components/PSNHeader.tsx
+++ components/PSNHeader.tsx
@@ -1,10 +1,33 @@
 import { FC } from "react";
 import styles from "@/styles/PSNHeader.module.css";
+import { useRouter } from "next/router";
+import Input from "./Input";
 
-const PSNHeader: FC = () => (
-  <header className={styles.header}>
-    <p>PSNHeader</p>
-  </header>
-);
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
+const PSNHeader: FC = () => {
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    fetch(`${API_URL}/psn/signOut`)
+      .then(() => {
+        router.reload();
+      })
+      .catch((error) => {
+        console.error("sign out error", error);
+      });
+  };
+
+  return (
+    <header className={styles.header}>
+      <Input
+        className={styles.button}
+        onClick={() => handleSignOut()}
+        type="button"
+        value="Sign out"
+      />
+    </header>
+  );
+};
 
 export default PSNHeader;
